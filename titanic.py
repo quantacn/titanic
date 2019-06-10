09c22113030fb8bca0d6b11ab1dff4b745b30eeb
ked <874433188@qq.com>
2019-06-10 22:37:15 +0800

    201906102237

--- titanic.py
+++ titanic.py
@@ -37,8 +37,8 @@ combine = [train_df, test_df]
 #Pclass_Survived_1 = train_df.Pclass[train_df['Survived'] == 1].value_counts()
 #Pclass_Survived = pd.DataFrame({ 0: Pclass_Survived_0, 1: Pclass_Survived_1})
 #删除列的时候必须制定axis=1
-train_df=train_df.drop(['Name','PassengerId'], axis=1)
-test_df = test_df.drop(['Name'], axis=1)
+train_df=train_df.drop(['Name','PassengerId','Ticket'], axis=1)
+test_df = test_df.drop(['Name','Ticket'], axis=1)
 age_guess = train_df['Age'].median()
 train_df.loc[train_df['Age'].isnull(),'Age']=age_guess
 #train_df['Age'].fillna(age_guess)
@@ -46,6 +46,6 @@ Fare_guess=train_df['Fare'].mode()
 train_df['Fare'].fillna(Fare_guess)
 train_df=train_df.drop('Cabin',axis=1)
 train_df['Sex']=train_df['Sex'].map({'female':0,'male':1}).astype(int)
-
-print (train_df.info())
+#SibSp Parch列合并成新列，然后处理数据，分为三类。年龄亦如此。
+print (train_df.head())
 
